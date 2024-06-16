6dcea0c403c036873b487e70027d61ba4e7080f3
Marttoni <166274576+Marttoni@users.noreply.github.com>
2024-06-16 20:00:53 -0300

    Atualização Cadastro

--- TCadastro.dfm
+++ TCadastro.dfm
@@ -1,7 +1,7 @@
 object Form2: TForm2
   Left = 0
   Top = 0
-  Caption = 'Form2'
+  Caption = 'Cadastro'
   ClientHeight = 442
   ClientWidth = 628
   Color = clBtnFace
@@ -11,4 +11,69 @@ object Form2: TForm2
   Font.Name = 'Segoe UI'
   Font.Style = []
   TextHeight = 15
+  object Label1: TLabel
+    Left = 76
+    Top = 61
+    Width = 24
+    Height = 15
+    Caption = 'CPF:'
+  end
+  object Label2: TLabel
+    Left = 63
+    Top = 95
+    Width = 37
+    Height = 15
+    Caption = 'E-Mail:'
+  end
+  object Label3: TLabel
+    Left = 8
+    Top = 153
+    Width = 92
+    Height = 15
+    Caption = 'Confirmar Senha:'
+  end
+  object Label4: TLabel
+    Left = 65
+    Top = 124
+    Width = 35
+    Height = 15
+    Caption = 'Senha:'
+  end
+  object EdCPF: TEdit
+    Left = 106
+    Top = 58
+    Width = 121
+    Height = 23
+    TabOrder = 0
+  end
+  object EdEmail: TEdit
+    Left = 106
+    Top = 92
+    Width = 121
+    Height = 23
+    TabOrder = 1
+  end
+  object EdSenha: TEdit
+    Left = 106
+    Top = 121
+    Width = 121
+    Height = 23
+    TabOrder = 2
+  end
+  object EdCSenha: TEdit
+    Left = 106
+    Top = 150
+    Width = 121
+    Height = 23
+    TabOrder = 3
+  end
+  object BtCadastrar: TButton
+    Left = 120
+    Top = 192
+    Width = 75
+    Height = 25
+    Caption = 'Cadastrar'
+    TabOrder = 4
+    OnClick = BtCadastrarClick
+  end
 end
--- TCadastro.pas
+++ TCadastro.pas
@@ -4,21 +4,66 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
-type
+type Cadastro = record
+	cpf,senha,cSenha:integer;
+	email:string[200];
+end;
+
   TForm2 = class(TForm)
-  private
-    { Private declarations }
-  public
-    { Public declarations }
-  end;
+    EdEmail: TEdit;
+		EdSenha: TEdit;
+    EdCSenha: TEdit;
+		Label1: TLabel;
+		Label2: TLabel;
+		Label3: TLabel;
+		Label4: TLabel;
+    EdCPF: TEdit;
+    BtCadastrar: TButton;
+    procedure BtCadastrarClick(Sender: TObject);
+
+
+	private
+		{ Private declarations }
+		ArrayCadastro: array of Cadastro;
+	public
+		{ Public declarations }
+	end;
 
 var
-  Form2: TForm2;
+	Form2: TForm2;
 
 implementation
 
 {$R *.dfm}
+
+{ TForm2 }
+procedure TForm2.BtCadastrarClick(Sender: TObject);
+var
+	NovoCadastro: Cadastro;
+	i:integer;
+begin
+//Recebe os dados informados no cadastro
+	NovoCadastro.cpf := StrToIntDef (EdCPF.Text, 0);
+	NovoCadastro.email := EdEmail.text;
+	NovoCadastro.senha := StrToIntDef (EdSenha.Text, 0);
+	NovoCadastro.cSenha := StrToIntDef (EdCSenha.Text, 0);
+
+//Verifica se as senhas são correspondentes,caso não sejam o cadastro não é realizado
+	if NovoCadastro.senha <> NovoCadastro.cSenha then
+	begin
+		ShowMessage('As senhas devem ser iguais');
+		exit;
+	end;
+
+//Redimensionar o array para receber novo cadastro
+	i := length(ArrayCadastro);
+	SetLength(ArrayCadastro, i + 1);
+	ArrayCadastro[i] := NovoCadastro;
+
+// Mensagem de confirmação
+	ShowMessage('Cadastro Realizado!');
+end;
 
 end.
